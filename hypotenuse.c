d59147b9edcc2483848a6bd163e2339c2ef2890d
Matt-Ferraz <helinhorx2016@gmail.com>
2023-02-24 15:11:37 -0300

    added circle and hypotenuse operations

---
+++ hypotenuse.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  char unit[10] = "";
+  float a, b, c;
+
+  printf("Well, first of all, specify the unit you are using: ");
+  scanf("%s", unit);
+
+  
+
+  return 0;
+};
